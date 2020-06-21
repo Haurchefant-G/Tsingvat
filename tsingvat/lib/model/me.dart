7f8a71de33d98065d027977cdb62d86bbe528a6a
LittleHealth <zhxj0818@163.com>
2020-06-21 21:42:41 +0800

    add chat file

---
+++ tsingvat/lib/model/me.dart
@@ -0,0 +1,17 @@
+class Profile {
+  String name;
+  String avatar;
+  String account;
+
+  Profile({
+    this.name,
+    this.account,
+    this.avatar
+  });
+}
+
+Profile me = new Profile(
+    name: 'wkiwi',
+    avatar:'https://avatars3.githubusercontent.com/u/35719531?s=460&v=4',
+    account: 'WZ610935700'
+);
