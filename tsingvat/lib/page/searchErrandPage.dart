183548f1951f68c21f0c9d9a16f286fe8a5ff33a
Haurchefant-G <guiacanray@gmail.com>
2020-06-23 18:59:15 +0800

    tsingvat_1.4.3

---
+++ tsingvat/lib/page/searchErrandPage.dart
@@ -0,0 +1,324 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tsingvat/component/customDiaglog.dart';
+import 'package:tsingvat/component/errandcard.dart';
+import 'package:tsingvat/const/code.dart';
+import 'package:tsingvat/model/errand.dart';
+import 'package:tsingvat/util/GradientUtil.dart';
+import 'package:tsingvat/util/SharedPreferenceUtil.dart';
+import 'package:tsingvat/util/httpUtil.dart';
+
+class searchErrandPage extends StatefulWidget {
+  @override
+  _searchErrandPageState createState() => _searchErrandPageState();
+}
+
+class _searchErrandPageState extends State<searchErrandPage> {
+  GlobalKey<FormState> searchKey = GlobalKey<FormState>();
+  FocusNode strlikeFocus = FocusNode();
+  FocusNode minBonusFocus = FocusNode();
+  FocusNode maxBonusFocus = FocusNode();
+
+  HttpUtil http;
+  List<Errand> results = [];
+  bool load = false;
+  bool current;
+
+  String strlike;
+  String fromAddr;
+  String toAddr;
+  String minBonus;
+  String maxBonus;
+
+  List<DropdownMenuItem<String>> locationItems = [
+    '第六教学楼',
+    '第三教学楼',
+    '第二教学楼',
+    '第四教学楼',
+    '桃李园周边',
+    '紫荆园周边',
+    '清芬园周边',
+    '李兆基大楼',
+    '东操周边',
+    '主楼周边',
+    '西操周边',
+    '北馆',
+    '快递点',
+    '北门',
+    '东南门',
+    '西门',
+    '其他'
+  ].map<DropdownMenuItem<String>>((String v) {
+    return DropdownMenuItem<String>(
+      value: v,
+      child: Text(v),
+    );
+  }).toList();
+
+  @override
+  void initState() {
+    super.initState();
+    http = HttpUtil();
+    current = true;
+  }
+
+  Future<void> _Search() async {
+    var data;
+    strlikeFocus.unfocus();
+    minBonusFocus.unfocus();
+    maxBonusFocus.unfocus();
+    setState(() {
+      results.clear();
+      load = true;
+    });
+    try {
+      data = await http.get('/errand', {
+        "strlike": strlike,
+        "fromAddr": fromAddr,
+        "toAddr": toAddr,
+        "minBonus": minBonus,
+        "maxBonus": maxBonus
+      });
+      await Future.delayed(Duration(milliseconds: 500), () {});
+    } catch (e) {
+      print(e);
+    }
+    print(data);
+    if (data['code'] == ResultCode.SUCCESS) {
+      for (var json in data['data']) {
+        var e = Errand.fromJson(json);
+        //errands.add(e);
+        if (e.taker == null) {
+          results.add(e);
+        }
+      }
+    }
+    if (current == true) {
+      setState(() {
+        load = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    current = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("123"),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Form(
+                key: searchKey,
+                child: DefaultTextStyle(
+                  style: Theme.of(context).textTheme.subtitle1,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.all(8.0)),
+                      Container(
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(100)),
+                          color: Theme.of(context).dialogBackgroundColor,
+                        ),
+                        child: TextFormField(
+                          focusNode: strlikeFocus,
+                          decoration: InputDecoration(
+                            hintText: '任务关键字',
+                            prefixIcon: Icon(Icons.local_offer),
+                            border: InputBorder.none,
+                          ),
+                          onEditingComplete: () {
+                            strlikeFocus.unfocus();
+                          },
+                          keyboardType: TextInputType.text,
+                          onSaved: (v) {
+                            strlike = v;
+                          },
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.all(8.0)),
+                      Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(100)),
+                                color: Theme.of(context).dialogBackgroundColor,
+                              ),
+                              child: TextFormField(
+                                focusNode: minBonusFocus,
+                                decoration: InputDecoration(
+                                  hintText: '最低报酬',
+                                  prefixIcon: ImageIcon(
+                                      AssetImage("assets/icon/RMB.png")),
+                                  border: InputBorder.none,
+                                ),
+                                inputFormatters: [
+                                  WhitelistingTextInputFormatter(
+                                      RegExp("[0-9]"))
+                                ],
+                                keyboardType: TextInputType.numberWithOptions(
+                                    signed: false, decimal: true),
+                                onEditingComplete: () {
+                                  minBonusFocus.unfocus();
+                                },
+                                onSaved: (v) {
+                                  minBonus = v;
+                                  // try {
+                                  //   minBonus = v;
+                                  // } catch (e) {
+                                  //   errand.bonus = -1;
+                                  // }
+                                },
+                              ),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(8)),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 45,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(100)),
+                                color: Theme.of(context).dialogBackgroundColor,
+                              ),
+                              child: TextFormField(
+                                focusNode: maxBonusFocus,
+                                decoration: InputDecoration(
+                                  hintText: '最高报酬',
+                                  prefixIcon: ImageIcon(
+                                      AssetImage("assets/icon/RMB.png")),
+                                  border: InputBorder.none,
+                                ),
+                                inputFormatters: [
+                                  WhitelistingTextInputFormatter(
+                                      RegExp("[0-9]"))
+                                ],
+                                keyboardType: TextInputType.numberWithOptions(
+                                    signed: false, decimal: true),
+                                onEditingComplete: () {
+                                  maxBonusFocus.unfocus();
+                                },
+                                onSaved: (v) {
+                                  maxBonus = v;
+                                  // try {
+                                  //   errand.bonus = double.parse(v);
+                                  // } catch (e) {
+                                  //   errand.bonus = -1;
+                                  // }
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.all(8.0)),
+                      Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 45,
+                              padding: EdgeInsets.only(right: 8),
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(100)),
+                                color: Theme.of(context).dialogBackgroundColor,
+                              ),
+                              child: DropdownButtonFormField<String>(
+                                  decoration: InputDecoration(
+                                    hintText: '取物地点',
+                                    prefixIcon: Icon(Icons.location_city),
+                                    border: InputBorder.none,
+                                  ),
+                                  icon: Icon(Icons.arrow_downward),
+                                  value: fromAddr,
+                                  items: locationItems,
+                                  onChanged: (String v) {
+                                    fromAddr = v;
+                                  }),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(8)),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              height: 45,
+                              padding: EdgeInsets.only(right: 8),
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(100)),
+                                color: Theme.of(context).dialogBackgroundColor,
+                              ),
+                              child: DropdownButtonFormField<String>(
+                                  decoration: InputDecoration(
+                                    hintText: '送达地点',
+                                    prefixIcon: Icon(Icons.location_city),
+                                    border: InputBorder.none,
+                                  ),
+                                  icon: Icon(Icons.arrow_downward),
+                                  value: toAddr,
+                                  items: locationItems,
+                                  onChanged: (String v) {
+                                    toAddr = v;
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.all(20)),
+                    ],
+                  ),
+                )),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.grey[200],
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        onPressed: _Search,
+        child: Icon(Icons.done_outline),
+      ),
+      body: CustomScrollView(slivers: <Widget>[
+        SliverToBoxAdapter(
+          child: Padding(padding: EdgeInsets.all(10)),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, i) {
+            if (i == results.length) {
+              return load
+                  ? Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: SpinKitWave(
+                          color: Colors.lightBlueAccent.withOpacity(0.5),
+                          size: 30.0),
+                    )
+                  : Padding(padding: EdgeInsets.all(20));
+            }
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: ErrandCard(results[i], _Search),
+            );
+          }, childCount: results.length + 1),
+        ),
+      ]),
+    );
+  }
+}
--- tsingvat/lib/tabPage/ErrandPage.dart
+++ tsingvat/lib/tabPage/ErrandPage.dart
@@ -1,7 +1,9 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:tsingvat/component/errandcard.dart';
 import 'package:tsingvat/const/code.dart';
 import 'package:tsingvat/model/errand.dart';
+import 'package:tsingvat/page/searchErrandPage.dart';
 import 'package:tsingvat/util/httpUtil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -34,7 +36,7 @@ class _ErrandPageState extends State<ErrandPage> {
           _getMore();
         }
       });
-    _getMore();//.then((value) => _getMore());
+    _getMore(); //.then((value) => _getMore());
   }
 
   Future<void> _refresh() async {
@@ -117,55 +119,84 @@ class _ErrandPageState extends State<ErrandPage> {
   @override
   Widget build(BuildContext context) {
     print("build");
-    return RefreshIndicator( child :Container(
-      //padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      //padding: EdgeInsets.all(16),
-      // decoration: BoxDecoration(
-      //     color: Theme.of(context).backgroundColor,
-      //     borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
-          child: ListView.builder(
-              physics: AlwaysScrollableScrollPhysics(),
-              controller: _scrollController,
-              itemCount: errands.length + 2,
-              itemBuilder: (context, i) {
-                if (i == 0) {
-                  return Container(
-                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    decoration: BoxDecoration(
-                      boxShadow: [BoxShadow()],
-                      border: Border.all(width: 1, color: Colors.grey),
-                      borderRadius: BorderRadius.all(Radius.circular(100)),
-                      color: Theme.of(context).dialogBackgroundColor,
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
-                  );
-                } else if (i == errands.length + 1) {
-                  return nomore
-                      ? Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "没有更多了",
-                            textAlign: TextAlign.center,
+    return RefreshIndicator(
+      child: Container(
+        //padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        //padding: EdgeInsets.all(16),
+        // decoration: BoxDecoration(
+        //     color: Theme.of(context).backgroundColor,
+        //     borderRadius: BorderRadius.vertical(top: Radius.circular(15))),
+        child: ListView.builder(
+            physics: AlwaysScrollableScrollPhysics(),
+            controller: _scrollController,
+            itemCount: errands.length + 2,
+            itemBuilder: (context, i) {
+              if (i == 0) {
+                return OpenContainer(
+                  closedBuilder: (BuildContext _, VoidCallback openContainer) {
+                    return Stack(
+                      alignment: AlignmentDirectional.centerEnd,
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          decoration: BoxDecoration(
+                            boxShadow: [BoxShadow()],
+                            border: Border.all(width: 1, color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(100)),
+                            color: Theme.of(context).dialogBackgroundColor,
+                          ),
+                          child: null,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              color: Color.fromARGB(255, 126, 126, 126),
+                            ),
+                            onPressed: () {},
                           ),
                         )
-                      : (load
-                          ? Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: SpinKitWave(
-                                  color:
-                                      Colors.lightBlueAccent.withOpacity(0.5),
-                                  size: 30.0),
-                            )
-                          : Padding(padding: EdgeInsets.all(20), child: FlatButton(onPressed: _getMore, child: Text("点击加载更多")),));
-                }
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: ErrandCard(errands[i - 1], _refresh),
+                      ],
+                    );
+                  },
+                  closedElevation: 0,
+                  transitionType: ContainerTransitionType.fade,
+                  openBuilder: (BuildContext _, VoidCallback openContainer) {
+                      return searchErrandPage();
+                    },
                 );
-              }),),
-          onRefresh: _refresh,
+              } else if (i == errands.length + 1) {
+                return nomore
+                    ? Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "没有更多了",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    : (load
+                        ? Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: SpinKitWave(
+                                color: Colors.lightBlueAccent.withOpacity(0.5),
+                                size: 30.0),
+                          )
+                        : Padding(
+                            padding: EdgeInsets.all(20),
+                            child: FlatButton(
+                                onPressed: _getMore, child: Text("点击加载更多")),
+                          ));
+              }
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: ErrandCard(errands[i - 1], _refresh),
+              );
+            }),
+      ),
+      onRefresh: _refresh,
     );
 
 //Sliver版本
